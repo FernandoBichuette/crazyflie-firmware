ff746f426a8e34c055bf04d51f00ea820e293474
Fernando <fernandoba2@al.insper.edu.br>
2023-08-24 08:05:58 -0300

    Chap2

---
+++ programs/chapters/ch02.h
@@ -0,0 +1,10 @@
+#include "mbed.h"
+#include "crazyflie.h"
+#include "pin_names.h"
+#include "parameters.h"
+
+// Define all LEDs as digital output objects
+
+// Define all motors as PWM objects
+
+PwmOut motor4(MOTOR4);
