7d94f64fddaa1e0fe61a1edc92c706d039552a76
Fernando <fernandoba2@al.insper.edu.br>
2023-08-29 08:08:18 -0300

    Aula 3 final

--- programs/chapters/ch02.h
+++ programs/chapters/ch02.h
@@ -8,12 +8,20 @@ PwmOut motor4(MOTOR4);
 
 float p = 1.0/500.0;
 
+float control_motor(float omega){
+    float PWM=1.7439e-07*(omega*omega)+3.9258e-11*omega;
+    return PWM;
+}
+
+
 int main(){
 
     motor4.period(p);
+    motor4=control_motor(1000.0);
+    wait(10);
+    motor4=0.0;
 
     while(true){
-        motor4=1;
             
-        }
-    }        
+    }
+}        
