67b6a99800b6b549ce063da3a7d89ea3a1da8c69
Fernando <fernandoba2@al.insper.edu.br>
2023-10-31 15:59:06 -0300

    aula 16

---
+++ src/modules/attitude_controller.cpp
@@ -0,0 +1,15 @@
+#include "attitude_controller.h"
+
+//Class constructor
+
+AttitudeController::AttitudeController()
+{
+}
+
+void AttitudeController::control(float phi_r, float theta_r, float psi_r, float phi, float theta, float psi, float p, float q, float r)
+{
+}
+
+float AttitudeController::control_siso(float angle_r, float angle, float rate, float kp, float kd)
+{
+}
